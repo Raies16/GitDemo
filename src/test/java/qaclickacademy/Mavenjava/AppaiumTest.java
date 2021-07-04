81056f6c81012aa373349a2a2186c65d23fe5bb0
raies71 <raiesabdul@gmail.com>
2021-07-04 00:49:56 -0700

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppaiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppaiumTest {
+	
+	@Test
+	public void nativeApp() {
+		System.out.println("nativeApp");
+	}
+	
+	
+	@Test
+	public void iosApp() {
+		System.out.println("iosApp");
+	}
+
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+	
+	
+	@Test
+	public void deletedTwitter() {
+		System.out.println("deletedTwitter");
+	}
+}
