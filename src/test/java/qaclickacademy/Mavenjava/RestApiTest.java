303e802bde19abd3b606bef883764f0094924d69
raies71 <raiesabdul@gmail.com>
2021-07-04 03:08:48 -0700

    architect guy

--- src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -10,6 +10,10 @@ public class RestApiTest {
 		System.out.println("postJira1");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
 	}
 	
 	
